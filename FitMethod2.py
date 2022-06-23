Fix calculation difference in left and right
When no events in the files

FitMethod2.py
@@ -199,12 +199,19 @@ class FitMethod2:
         fC0  = np.fft.fft2(self.compton_fit_sum)
         fD0  = np.fft.fft2(self.data_sum)
         s = np.abs(np.sum(fC0))
-        k=1e-3
         k=0
+        fC0 = fC0/np.sum(np.abs(fC0))
+        fD0 = fD0/np.sum(np.abs(fD0))
         fB = fD0/(fC0 + k*s)
 
+        phi_fB = np.sum( np.angle(fB[0:self.shape[0]//2, 0:self.shape[1]//2]) )/(self.shape[0]*self.shape[1]/4)
+        print("average complex phase for fB degree", phi_fB/3.1415926*180.0)
         tmp =  np.abs(np.fft.ifft2(fB))
-        self.data_diff =  self.fft_fix(tmp)
+        if abs(phi_fB) < np.pi/2:
+            self.data_diff =  self.fft_fix(tmp)
+        else:
+            self.data_diff =  tmp
+        #self.data_diff= np.abs(fB)
         #self.data_sum = self.smooth(self.data_sum, 7)
 
 
@@ -257,6 +264,7 @@ class FitMethod2:
                                                         label=field_name+'_py',
                                                         data_type = this_data_type)
         data_field_dict['data_diff'].interpolation='bicubic'
+        #data_field_dict['data_sum'].interpolation='bicubic'
         return data_field_dict
 
     def fix(self, parlist):

pol_fit.py
@@ -134,11 +134,13 @@ def read_batch(hist_fpath, file_arr, vepp4E):
         env = D['env_params'].item()
         nl = int(sum(sum(D['hc_l'])))
         nr = int(sum(sum(D['hc_r'])))
+        delta_n = (nl-nr)/(nl+nr)*2 if (nl+nr)>0 else 0.0
+        delta_n_error = 4./(nl+nr)**2*np.sqrt(nl*nl*nr + nr*nr*nl) if (nl+nr) > 0 else 0.0
         print('{:>5} {:>30} {:12} {:12} {:>15} {:>15.2f} {:>15.2f} {:15.3f}'.format( 
             count, 
             filename, 
             nl , nr ,
-            '{:.2f} +- {:.2f}'.format( (nl-nr)/(nl+nr)*2*100., 4./(nl+nr)**2*np.sqrt(nl*nl*nr + nr*nr*nl)*100. ) ,
+            '{:.2f} +- {:.2f}'.format( delta_n*100., delta_n_error*100.) ,
             env['vepp4E'],
             env['vepp4H_nmr'],
             env['dfreq']